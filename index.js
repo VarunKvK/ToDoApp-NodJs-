2333e23712fab38d6883187d844bca8a4d7f4cf7
VarunKvK <varunkrishnankv2002@gmail.com>
2023-05-30 18:51:54 +0530

    ToDo App using Node updated by making the files more eaasy to use

--- index.js
+++ index.js
@@ -2,32 +2,48 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const daymodule=require(__dirname+"/date.js")
 
 const port = 3000;
-const app = express();
 
-var todoItems=[];
+const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"));
+
+const work=[]
+const todoItems=[];
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var options={
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  }
-    var current=today.toLocaleDateString("en-US",options)
+ const current= daymodule.getDay()
   res.render("list", { todayDay: current, todolists:todoItems});
 });
 
-app.post("/",(req,res)=>{
-    var todoItem=req.body.todo;
-    todoItems.push(todoItem)
-    console.log(todoItem)
-    res.redirect("/")
+app.get("/work",(req,res)=>{
+  res.render("list",{todayDay:"Work List",todolists:work})
 })
 
+app.get("/about",(req,res)=>{
+  res.render("about")
+})
+app.post("/",(req,res)=>{
+  const todoItem=req.body.todo;
+  if(req.body.button==="Work"){
+    work.push(todoItem);
+    res.redirect("/work")
+  }else{
+    todoItems.push(todoItem)
+    console.log(todoItem)
+    console.log(req.body)
+    res.redirect("/")
+  }
+  })
+
+app.post("/work",(req,res)=>{
+  const workItem=req.body.todo
+  work.push(workItem)
+  res.redirect("/work")
+})
 app.listen(port, () => {
   console.log("Server is running on port : " + port);
 });
